436c3e71d28ca45e474c20c583a20b32b0810d41
Sergei <112075321+sergegithub22@users.noreply.github.com>
2023-06-28 17:47:11 +0300

    280623
    1747

---
+++ assets/js/accordion.js
@@ -0,0 +1,1 @@
+for(var acc=document.getElementsByClassName("accordion_btn"),i=0;i<acc.length;i++)acc[i].addEventListener("click",function(){this.classList.toggle("active");var t=this.nextElementSibling;t.style.maxHeight?t.style.maxHeight=null:t.style.maxHeight=t.scrollHeight+"px"});
